d3326ca1fb9a36f85fd1cb09ecaf178b9c89589b
iamserkan <yosoyserkan@gmail.com>
2017-07-18 13:32:24 +0300

    asd

--- The Weather App/ViewController.swift
+++ The Weather App/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     let urlBase:String = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{QUERY}%22)%20and%20u%3D'c'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
     
     
-    // asd
+    // asd  
     
     @IBAction func getWeather(_ sender: Any) {
         
